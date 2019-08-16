d7ad4a81fc63ac1ed47838a6b9f5c0d5ff607932
Connor Meth <connormeth27@gmail.com>
2019-08-16 19:14:49 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 var recipes = new Object({});
 
 var obj = { prop: 1 };
-function updateObjectWithKeyAndValue(recipes, key, value) {
+function updateObjectWithKeyAndValue(object, key, value) {
  return Object.assign({}, obj, {[key]: value});
 }
 
-function destructivelyUpdateObjectWithKeyAndValue(recipes, key, value) {
-  recipes[key] = value;
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
+  object[key] = value;
   return recipes;
 }
